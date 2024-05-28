20969cb2313f3f58ab66ce21ad3eaed2c0741ed1
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2024-05-28 12:36:48 +0600

    07 - OOP Part 1 - Inheritance-090 - Inheritance - Part 3

--- 07 - OOP Part 1 - Inheritance/088 - Inheritance/Inherritance/src/Dog.java
+++ 07 - OOP Part 1 - Inheritance/088 - Inheritance/Inherritance/src/Dog.java
@@ -25,13 +25,39 @@ public class Dog extends Animal {
 
     @Override
     public void makeNoise() {
-        super.makeNoise();
+        if(type == "Wolf") {
+        System.out.print("Ow Wooooo! ");
+        }
+        bark();
+        System.out.println();
     }
     @Override
     public void move(String speed) {
         super.move(speed);
-        System.out.println("Dogs walk, run and wag their tail");
+//        System.out.println("Dogs walk, run and wag their tail");
+        if(speed == "slow"){
+            walk();
+            wagTail();
+        }else {
+            run();
+            bark();
+        }
+        System.out.println();
     }
 
+    private void bark(){
+        System.out.print("Woof! ");
+    }
+
+    private void run(){
+        System.out.println("Dog running ");
+    }
+
+    private void walk(){
+        System.out.println("Dog Walking ");
+    }
+    private void wagTail(){
+        System.out.println("Tail Wagging ");
+    }
 
 }
