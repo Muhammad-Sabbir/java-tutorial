e6ca4e62803df4965e17fd9c7437953590acc260
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2024-05-31 23:20:51 +0600

    07 - OOP Part 1 - Inheritance-092 - Inheritance Challenge - Part 1

---
+++ 07 - OOP Part 1 - Inheritance/092 - Inheritance Challenge/src/HourlyEmployee.java
@@ -0,0 +1,5 @@
+public class HourlyEmployee extends Employee{
+    private double hourlyPayRate;
+
+     
+}
---
+++ 07 - OOP Part 1 - Inheritance/092 - Inheritance Challenge/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee tim = new Employee("Tim", "1985-08-30", "2003-01-01");
+        System.out.println(tim.getAge());
+    }
+}
