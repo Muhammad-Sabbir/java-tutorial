d0dbfbe2bab92438cf64a9c730c6eee5fd60266a
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2024-06-13 12:15:36 +0600

    07 - OOP Part 2 - Polymorphism-108 - Polymorphism, Part 2

---
+++ 08 - OOP Part 2 - Polymorphism/Polymorphism/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie thisMovie = new Adventure("Star Wars");
+        thisMovie.watchMovie();
+
+        Adventure thisAdventure = new Adventure("Adventure Movie");
+        thisAdventure.watchMovie();
+
+        Movie theStaticMovie = Movie.getMovie("Adventure", "Star Wars");
+        theStaticMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            System.out.print("Enter Type (A for Adventure, C for Comedy, " + " S for Science Fiction, or Q to quit): ");
+            String type = s.nextLine();
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+    }
+}
