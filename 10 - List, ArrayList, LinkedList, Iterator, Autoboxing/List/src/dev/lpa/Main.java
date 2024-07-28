ff821ae34dcc4be8270e0556e753dfc46f7e2bb9
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2024-07-28 16:07:01 +0600

    10 - List, ArrayList, LinkedList, Iterator, Autoboxing-131 - List and ArrayList Part 1

---
+++ 10 - List, ArrayList, LinkedList, Iterator, Autoboxing/List/src/dev/lpa/Main.java
@@ -0,0 +1,31 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count){
+    public GroceryItem(String name){
+        this(name, "DAIRY", 1);
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE",6 );
+        groceryArray[2] = "5 oranges";
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+
+        groceryList.add(new GroceryItem("Butter"));
+        System.out.println(groceryList);
+
+        }
+}
